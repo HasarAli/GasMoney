8f7c11d41cef4475f347331f1198122e2e27b4b7
HasarAli <hasar.ali@protonmail.com>
2021-10-02 03:12:53 -0400

    created route GET route for form

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,5 +1,12 @@
+from flask import render_template, url_for
 from flaskr import app
+from flaskr.forms import RegistrationForm
 
 @app.route('/')
 def index():
     return '<h1>Hey There!</h1>'
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", form=form)
