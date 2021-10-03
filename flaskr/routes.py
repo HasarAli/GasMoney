bb816654ec4ba9b72041c9a8d79a9358f538c3b1
HasarAli <hasar.ali@protonmail.com>
2021-10-03 02:41:11 -0400

    server side form checking and redirect + feedback

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,12 +1,20 @@
-from flask import render_template, url_for
+from flask import render_template, redirect, flash
+from flask.helpers import url_for
 from flaskr import app
 from flaskr.forms import RegistrationForm
 
 @app.route('/')
 def index():
-    return '<h1>Hey There!</h1>'
+    return render_template("layout.html")
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash('Account Created Successfully', 'success')
+        return redirect(url_for('index'))
     return render_template("register.html", form=form)
+
+@app.route('/login')
+def login():
+    return '<h1>Log In</h1>'
