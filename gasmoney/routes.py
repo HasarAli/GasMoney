5314e2db54bf5e68eb0c570f468d9e55c7e2cc83
HasarAli <hasar.ali@protonmail.com>
2021-10-07 18:22:22 -0400

    ability to register to db

--- gasmoney/routes.py
+++ gasmoney/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, redirect, flash
 from flask.helpers import url_for
-from gasmoney import app
+from werkzeug.security import check_password_hash, generate_password_hash
+from gasmoney import app, db
 from gasmoney.forms import RegistrationForm
 from gasmoney.models import User, Ride, Reservation
 
@@ -12,8 +13,12 @@ def index():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        hashed_passowrd = generate_password_hash(form.password.data, method='pbkdf2:sha256', salt_length=16)
+        user = User(email=form.email.data, phone=form.phone.data, first_name=form.first_name.data, last_name=form.last_name.data, gender=form.gender.data, password=hashed_passowrd)
+        db.session.add(user)
+        db.session.commit()
         flash('Account Created Successfully', 'success')
-        return redirect(url_for('index'))
+        return redirect(url_for('login'))
     return render_template("register.html", form=form)
 
 @app.route('/login')
