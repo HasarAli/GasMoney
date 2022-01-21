274d6cef0027d271e215a3f04f70254dab61e99d
HasarAli <Hasar.Ali@protonmail.com>
2022-01-20 21:04:15 -0500

    implemented a new production entrypoint

--- gasmoney/__init__.py
+++ gasmoney/__init__.py
@@ -1,11 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from twilio.rest import Client as TwilioClient
 from sendgrid import SendGridAPIClient
+from os import getenv
 
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_object('config')
-app.config.from_pyfile('config.py')
+if getenv('ENV') == 'PROD':
+  app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+  app.config['SQLALCHEMY_DATABASE_URI'] = getenv('SQLALCHEMY_DATABASE_URI')
+  app.config['SEND_GRID_API_KEY'] = getenv('SEND_GRID_API_KEY')
+  app.config['TWILIO_API_KEY'] = getenv('TWILIO_API_KEY')
+  app.config['TWILIO_API_SECRET'] = getenv('TWILIO_API_SECRET')
+  app.config['TWILIO_ACCOUNT_SID'] = getenv('TWILIO_ACCOUNT_SID')
+  app.config['TWILIO_VERIFY_SERVICE'] = getenv('TWILIO_VERIFY_SERVICE')
+else:
+  app.config.from_object('config')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
