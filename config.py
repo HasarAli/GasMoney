b40e44bf3726dec02ee20dd63e542b12f2b5f68c
HasarAli <Hasar.Ali@protonmail.com>
2022-01-20 21:08:48 -0500

    updated config files

--- config.py
+++ config.py
@@ -1,3 +1,14 @@
 # Development Environment
 SECRET_KEY = "0106a27624e960346ad0a1415b56cac9"
 SQLALCHEMY_DATABASE_URI = "sqlite:///dev.db"
+
+# Get a valid api key
+# https://signup.sendgrid.com/'
+SEND_GRID_API_KEY = 'X'
+
+# Get info below by creating a free account at Twilio 
+# https://www.twilio.com/try-twilio
+TWILIO_API_KEY = 'X'
+TWILIO_API_SECRET = 'X'
+TWILIO_ACCOUNT_SID = 'X'
+TWILIO_VERIFY_SERVICE_SID = 'X'
